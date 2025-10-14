d68765c42ef7c4913ed9e5b1cfe59ac1487b1603
Martim Leme <up202304944@up.pt>
2025-10-14 18:32:26 +0100

    created two functions in frames.c

---
+++ headers/frames.h
@@ -0,0 +1,6 @@
+#ifndef _FRAMES_H_
+#define _FRAMES_H_
+
+int create_SET(unsigned char *frame); 
+int create_UA(unsigned char *frame); 
+#endif
---
+++ src/frames.c
@@ -0,0 +1,23 @@
+#include "../headers/frames.h"
+#include "../macros/const.h"
+
+
+int create_SET(unsigned char *frame) {
+    
+    frame[0] = FLAG;
+    frame[1] = A_SND;
+    frame[2] = C_SND;
+    frame[3] = BCC_SND;
+    frame[4] = FLAG;
+    return 5;
+
+}
+
+int create_UA(unsigned char * frame) {
+    frame[0] = FLAG; 
+    frame[1] = A_RCV; 
+    frame[2] = C_RCV;
+    frame[3] = BCC_RCV; 
+    frame[4] = FLAG; 
+    return 5;
+}
